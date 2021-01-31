43f480bcae7365e7a84b8b35f7a01db5cf82f978
atumv <atumv.gm@gmail.com>
2021-01-31 23:26:34 +0700

    Updates

---
+++ precache-manifest.53a528134df2857791fc1ae7cf663430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb6c6f64281cd66634f7d3fb7e71ccd",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "3e22517bcdad28b6be3a",
+    "url": "/movie-db/static/css/main.40fe0584.chunk.css"
+  },
+  {
+    "revision": "a85d9affd5d2136da89f",
+    "url": "/movie-db/static/js/2.306867da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-db/static/js/2.306867da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e22517bcdad28b6be3a",
+    "url": "/movie-db/static/js/main.3779d703.chunk.js"
+  },
+  {
+    "revision": "77986a98efb9fc2cf290",
+    "url": "/movie-db/static/js/runtime-main.c1a0928b.js"
+  }
+]);
