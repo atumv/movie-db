42526627a7c861ca370e84e2689b6f8ce073dd82
atumv <atumv.gm@gmail.com>
2022-04-23 21:13:37 +0700

    Add clapper icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import clapper from '../assets/svg/clapper.svg';
 
 export const Header = () => (
   <h1 className="header">
     <a className="header-link" href="index.html">
-      Movie Db 🎬
+      Movie Db <img className="header-clapper" src={clapper} alt="clapper" />
     </a>
   </h1>
 );
